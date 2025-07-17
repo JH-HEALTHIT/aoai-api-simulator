0e12544fe142f54bbab951f32d574e5a7c6374bb
Zackarya Faci <zfaci1@jh.edu>
2025-07-17 14:32:01 -0400

    change 4.o to 4o

--- src/aoai-api-simulator/src/aoai_api_simulator/generator/model_catalogue.py
+++ src/aoai-api-simulator/src/aoai_api_simulator/generator/model_catalogue.py
@@ -2,7 +2,7 @@ from aoai_api_simulator.models import OpenAIChatModel, OpenAIEmbeddingModel, Ope
 
 model_catalogue = {
     "gpt-4.1": OpenAIChatModel(name="gpt-4.1"),
-    "gpt-4.o": OpenAIChatModel(name="gpt-4.o"),
+    "gpt-4o": OpenAIChatModel(name="gpt-4o"),
     "o4-mini": OpenAIChatModel(name="o4-mini"),
     "gpt-3.5-turbo": OpenAIChatModel(name="gpt-3.5-turbo"),
     "gpt-3.5-turbo-0613": OpenAIChatModel(name="gpt-3.5-turbo-0613"),
