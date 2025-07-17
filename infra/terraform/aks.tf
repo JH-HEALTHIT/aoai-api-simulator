53f828757e58d31974a8e005bc6233f877facb9c
Zach Deering <Zach.Deering@jhu.edu>
2025-07-17 12:08:39 -0400

    [HGR-1015] Added a JH-specific readme, removed the helm chart from tf

--- infra/terraform/aks.tf
+++ infra/terraform/aks.tf
@@ -7,35 +7,6 @@ resource "kubernetes_namespace" "hgr-sim" {
   }
 }
 
-resource "helm_release" "hgr-aoai-simulator" {
-  name      = "hgr-aoai-simulator"
-  namespace = kubernetes_namespace.hgr-sim.metadata[0].name
-  chart     = "../helm/aoaisim"
-
-  values = [
-    templatefile("${path.module}/templates/sim-values.tftpl", {
-      st_rg           = azurerm_resource_group.rg.name,
-      st_name         = azurerm_storage_account.storage.name,
-      st_key          = azurerm_storage_account.storage.primary_access_key,
-      st_share        = azurerm_storage_share.simulator.name,
-      openai_endpoint = data.azurerm_cognitive_account.openai.endpoint,
-      image_tag       = "1.11.0",
-      ingress_class   = "nginx-dev",
-      hostname        = "hgr-aoaisim-dev.pm.jh.edu",
-      tenant_id       = data.azurerm_client_config.current.tenant_id,
-      client_id       = data.azurerm_kubernetes_cluster.pmap.kubelet_identity[0].client_id,
-      kv_name         = azurerm_key_vault.kv.name
-    })
-  ]
-
-  depends_on = [
-    kubernetes_namespace.hgr-sim,
-    azurerm_key_vault_secret.app_insights,
-    azurerm_key_vault_secret.openai_key,
-    azurerm_key_vault_secret.simulator_api_key
-  ]
-}
-
 ##-----------------------------------------------------------------------------
 ## Role assignments
 ##-----------------------------------------------------------------------------
